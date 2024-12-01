39fb73b6c74215afd821faa05fc54d7a4623317c
Martin Kandlinger <martinkandlingergmail.com>
2024-12-01 15:48:33 +0100

    plots angepasst & auf model_data input übersetzt

--- code/Plots Zwischenpräsentation.R
+++ code/Plots Zwischenpräsentation.R
@@ -10,7 +10,7 @@ theme.adjusted <- theme(axis.text.x = element_text(angle = 0, hjust = 0.5, margi
                         axis.text.y = element_text(hjust = 1, margin = margin(r = 10), size = 15, angle = 0),
                         axis.title.y = element_text(margin = margin(r = 20), size = 22),
                         title = element_text(color = "black"),
-                        plot.title = element_text(size = 28, color = "black", face = "bold", hjust = 0), 
+                        plot.title = element_text(size = 28, color = "black", face = "bold", hjust = 0.4), 
                         plot.subtitle = element_text(size = 17, color = "black", face = "italic"),
                         panel.grid.major = element_line(color = "black", linewidth = 0.1), 
                         panel.grid.minor = element_line(color = "gray", linewidth  = 0.1),
@@ -29,6 +29,7 @@ age_counts <- unique_patients %>%
                         labels = c("18-30", "31-40", "41-50", "51-60", "61-70", "71-80", "81-90", "91-102"))) %>% 
   count(AgeGroup)
 
+# Verteilung Alter ####
 # Barplot mit Count-Anzeige
 ggplot(age_counts, aes(x = AgeGroup, y = n)) +
   geom_bar(stat = "identity", fill = "skyblue", color = "black", alpha = 0.7) +
@@ -38,14 +39,22 @@ ggplot(age_counts, aes(x = AgeGroup, y = n)) +
   theme.adjusted + 
   theme(axis.text.y = element_text(hjust = 1, margin = margin(r = 10), size = 18, angle = 0))
 
+# mit model_data (ein Balken zeigt Anz. Patienten in 2er Gruppen -> 18&19 Jährige ein Balken, usw.):
+ggplot(model_data, aes(x = Age)) +
+  geom_histogram(binwidth = 2, fill = "#56B4E9", color = "black", alpha = 0.8) +
+  labs(title = "Verteilung Alter", x = "Alter", y = "Anzahl Patienten") +
+  scale_x_continuous(breaks = seq(20, 100, by = 10)) +
+  theme.main + theme.adjusted
 
-# Boxplot für ApacheIIScore
-ggplot(data_EK, aes(x = "", y = ApacheIIScore)) +
+
+# ApacheIIScore ####
+# Boxplot
+ggplot(model_data, aes(x = "", y = ApacheIIScore)) +
   # Boxplot zeichnen
   geom_boxplot(fill = "darkgrey", color = "black", coef = 1.5, alpha = 0.7, outlier.size = 3, outlier.shape = 16, lwd = 0.8) +
   
   # Mittelwert als roten Punkt anzeigen
-  stat_summary(fun = mean, geom = "point", shape = 16, size = 7, color = "#FF6666") +
+  stat_summary(fun = mean, geom = "point", shape = 16, size = 6, color = "#FF6666") +
   
   # Median als Text anzeigen
   stat_summary(fun = median, geom = "text", aes(label = ..y..), vjust = -1.2, size = 5.2, color = "black") +
@@ -60,15 +69,16 @@ ggplot(data_EK, aes(x = "", y = ApacheIIScore)) +
 # oder Histogram
 # binwidth = 2 bedeutet, dass jede Klasse (bin) eine Breite von 2 Einheiten hat.
 # [10, 12), [12, 14), [14, 16), ..., [58, 60]
-ggplot(unique_patients, aes(x = ApacheIIScore)) +
+ggplot(model_data, aes(x = ApacheIIScore)) +
   geom_histogram(binwidth = 2, fill = "skyblue", color = "black", alpha = 0.7) +
+  scale_x_continuous(breaks = seq(0, 80, by = 10)) +
   labs(title = "Verteilung des ApacheIIScore", 
        x = "ApacheIIScore", 
        y = "Häufigkeit") +
   theme.main + 
   theme.adjusted 
 
-# Balkendiagramm für Gender
+# Balkendiagramm für Gender ####
 ggplot(unique_patients, aes(x = Gender, fill = Gender)) +
   geom_bar(color = "black", alpha = 0.7) +
   labs(title = "Geschlechterverteilung", 
@@ -82,38 +92,23 @@ ggplot(unique_patients, aes(x = Gender, fill = Gender)) +
   theme(legend.text = element_text(size = 20), 
         legend.title = element_text(size = 25, face = "bold"), 
         legend.background = element_rect(fill = "white", color = "black", linewidth = 0.5),
-        legend.position = "bottom") 
-  
-  
+        legend.position = "bottom")
 
-# Anteil Verstorbene vs. Entlassene berechnen
-outcome_counts <- unique_patients %>%
-  summarise(
-    Verstorben = sum(PatientDied, na.rm = TRUE),
-    Entlassen = sum(PatientDischarged, na.rm = TRUE)
-  ) %>%
-  pivot_longer(cols = everything(), names_to = "Outcome", values_to = "Count")
+# überarbeitet (fand legende und x-Achsenlabel unnötig)
+ggplot(model_data, aes(x = Sex, fill = Sex)) +
+  geom_bar(color = "black", alpha = 0.7) +
+  labs(title = "Geschlechterverteilung", x = NULL, y = "Anzahl Patienten") +
+  scale_x_discrete(labels = c("Weiblich", "Männlich")) +
+  scale_fill_manual(values = c("Female" = "tomato", "Male" = "skyblue")) +
+  theme.main + theme.adjusted + theme(legend.position = "none")
 
-# Balkendiagramm erstellen
-ggplot(outcome_counts, aes(x = Outcome, y = Count, fill = Outcome)) +
-  geom_bar(stat = "identity", color = "black", alpha = 0.7) +
-  labs(title = "Anteil von Verstorbenen vs. Entlassenen", x = "Outcome", y = "Anzahl Patienten") +
-  scale_fill_manual(
-    values = c("Entlassen" = "chocolate1", "Verstorben" = "red")) +
-  guides(fill = guide_legend(override.aes = list(shape = 15, size = 8))) +
-  theme.main + 
-  theme.adjusted + 
-  theme(legend.text = element_text(size = 20), 
-        legend.title = element_text(size = 25, face = "bold"), 
-        legend.background = element_rect(fill = "white", color = "black", linewidth = 0.5),
-        legend.position = "bottom") 
 
-# Mit model_data:
+# Verstorbene vs. Entlassene vs. im Krankenhaus ####
 ggplot(model_data, aes(x = surv_icu_status_exp)) +
-  geom_bar(fill = "#56B4E9", color = "black") +
+  geom_bar(fill = "#56B4E9", color = "black", alpha = 0.8) +
   geom_text(stat = "count", 
             aes(label = paste0(round(..count../sum(..count..) * 100, 1), "%")),
-            vjust = 1.2, color = "black") +
+            vjust = 1.5, color = "black") +
   scale_x_discrete(labels =c("PatientDied" = "Verstorbene",
                              "PatientDischarged" = "Entlassene",
                              "PatientHospital" = "Im Krankenhaus")) +
@@ -165,6 +160,7 @@ surv_object_tod <- Surv(time = unique_patients_kaplan$surv_icu0to60, event = uni
 # Kaplan-Meier-Modell für Tod anpassen
 km_fit_tod <- survfit(surv_object_tod ~ 1)
 
+# ggsurvplot ####
 ggsurvplot(
   km_fit_tod,
   data = unique_patients_kaplan,
@@ -191,9 +187,9 @@ ggsurvplot(
 )
 
 
-
+# Beziehung zwischen Alter und ApacheIIScore ####
 # Scatterplot: Zeigt die Beziehung zwischen Alter und ApacheIIScore.
-ggplot(data_EK, aes(x = Age, y = ApacheIIScore)) +
+ggplot(model_data, aes(x = Age, y = ApacheIIScore)) +
   geom_point(alpha = 0.5, color = "black") +  # Punkte plotten
   geom_smooth(method = "lm", color = "red", se = TRUE) +  # Lineare Trendlinie mit Konfidenzintervall
   labs(
@@ -207,28 +203,7 @@ ggplot(data_EK, aes(x = Age, y = ApacheIIScore)) +
   theme.main + 
   theme.adjusted
 
-# Plot Propofol-Nutzung
-# Daten vorbereiten: Propofol-Werte summieren
-propofol_usage <- data_EK %>%
-  mutate(Propofol = as.numeric(as.character(Propofol))) %>% # Faktor in numerisch umwandeln
-  group_by(CombinedID) %>%
-  summarise(
-    total_propofol = sum(Propofol, na.rm = TRUE) # Summe von Propofol
-  ) %>%
-  ungroup()
-
-# Balkendiagramm erstellen
-ggplot(propofol_usage, aes(x = factor(total_propofol))) +
-  geom_bar(fill = "#56B4E9", color = "black", alpha = 0.8) + # Balken mit Hellblau und schwarzem Rand
-  labs(
-    title = "Propofol-Nutzung pro Patient",
-    x = "Anzahl der Propofol-Tage",
-    y = "Anzahl der Patienten"
-  ) +
-  theme.main + 
-  theme.adjusted
-
-# Vergleich mit model_data aus main.Rmd:
+# Plot Propofol-Tage ####
 ggplot(model_data, aes(x = factor(Days_Propofol))) +
   geom_bar(fill = "#56B4E9", color = "black", alpha = 0.8) +
   labs(
@@ -236,4 +211,3 @@ ggplot(model_data, aes(x = factor(Days_Propofol))) +
     x = "Anzahl der Propofol-Tage",
     y = "Anzahl der Patienten"
   ) + theme.main + theme.adjusted
-# Nice ist 1zu1 gleich
